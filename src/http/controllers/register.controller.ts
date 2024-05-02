83b7a069a5f05573a470e2b1ce8685843e7b13ef
JoaoKremerDev <joaokremer.dev@gmail.com>
2024-05-02 17:19:13 -0300

    feat: apply dependencie inversion at register controller

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,6 +1,8 @@
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { PrismaUsersRepository } from '../../repositories/prisma/prisma-users-repository'
+import { RegisterUseCase } from '@/use-cases/register'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
-import { registerUseCase } from '@/use-cases/register'
 
 export async function Register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -11,13 +13,19 @@ export async function Register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
   try {
-    await registerUseCase({
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    await registerUseCase.execute({
       name,
       email,
       password,
     })
   } catch (err) {
-    return reply.status(409).send('E-mail already exists')
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message })
+    }
+    throw err
   }
   return reply.status(201).send()
 }
