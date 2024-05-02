ff030a5bb530b6385834ddccb13cbf155a6b2bb3
JoaoKremerDev <joaokremer.dev@gmail.com>
2024-05-02 20:40:58 -0300

    feat: sessions route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from 'fastify'
 import { Register } from './controllers/register.controller'
+import { Authenticate } from './controllers/authenticate'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', Register)
+
+  app.post('/sessions', Authenticate)
 }
