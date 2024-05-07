f1ccbcdab12f86877c44490cc241513054fa15d4
JoaoKremerDev <joaokremer.dev@gmail.com>
2024-05-06 21:06:12 -0300

    feat(gyms-repo): new methods in repo

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,7 +1,14 @@
-import { Gym } from '@prisma/client'
+import { Gym, Prisma } from '@prisma/client'
+
+export interface FindManyNearbyParams {
+  latitude: number
+  longitude: number
+}
 
 export interface GymsRepository {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  items: any
+  items: Gym[]
   findById(id: string): Promise<Gym | null>
+  create(data: Prisma.GymCreateInput): Promise<Gym>
+  searchMany(query: string, page: number): Promise<Gym>
+  findManyNearby(params: FindManyNearbyParams): Promise<Gym[]>
 }
